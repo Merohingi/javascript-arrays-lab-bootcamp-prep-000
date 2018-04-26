1c048f56455afe56caa3ed9402b5b3328cd7e4d5
Marvin Bonds <mbonds1@mail.depaul.edu>
2018-04-26 13:27:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = "I don't do much."
 
-function addElementToEndOfArray(array, element) {
-  var newArray = [...array, element]
+function destructivelyAppendKitten(name) {
+  var newArray = [name]
   return newArray
 }
